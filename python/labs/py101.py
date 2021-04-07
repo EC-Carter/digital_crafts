01fed2180292cb3ac9be3773c3bdf3e7721417d7
EC Carter <eca21988@gmail.com>
2021-04-07 18:41:31 -0400

    initial upload for python labs

---
+++ python/labs/py101.py
@@ -0,0 +1,146 @@
+# print ("hello world")
+# print ("Carol "+ "Carter")
+# print ("\tThis is a great day")
+
+# found_coins = 20
+# magic_coins = 10
+# stolen_coins = 3
+# result = found_coins + magic_coins * 365 - stolen_coins * 52
+# print(result)
+
+# first_name = "Carol"
+# last_name = "Carter"
+# result = "Hello {1} {0} {0} {1}".format(first_name, last_name)
+#result = "Hello  {first} ".format(first=first_name, last=last_name) 
+# print(type(3.14)) 
+
+# print(isinstance(last_name, str))   
+
+#name = input("What is your name?:  ")
+#print(f"hello {name}")
+#print(type(name))
+
+# a = int(input("first value: "))
+# b = int(input("first value: "))
+# c = a + b
+# print(f"The value of {a} + {b} = {c} ")
+
+# user_wide = 6    #input("How many wide?: ")
+# user_tall = 4    #input("How many wide?: ")
+# a = "*"
+# x = " "
+# picture = False
+# if != picture:
+# counter = 0
+# while (counter < user_wide -1 ):
+#     print(a, end = '')
+#     counter += 1
+
+# counter2 = 0
+# while (counter2 < user_tall -1):
+#     print(a, end='')
+#     counter2+= 1
+
+# counter4 = 0
+# while (counter4 < user_wide -2):
+#     print ("x"*2)
+#     counter4 += 1
+
+# counter3 = 0
+# while (counter3 < user_wide ):
+#     print(a, end = '')
+#     counter3 += 1
+
+
+
+
+
+# try this agian at break
+
+# print(" ",a)
+# counter2 = 3
+# while(counter2 < 8 and counter2 % 2 == 1):
+#    print(" ",a*(counter2))
+# counter2 += 2
+
+
+
+# age = int(input("How old are you?: "))
+
+# if age >= 21:
+#    print("You get a free beer")
+# elif age < 18:
+#    print("Why are you at a bar")
+# else:
+#    print("Sorry, no beer for you")
+
+
+# count = 0
+# while count < 10:
+#    count += 1
+#    print(f"the count is {count}")
+
+
+# answer = ''
+
+# while answer != 'when':
+#    answer = input("Say when: ")
+#    answer = answer.lower()
+# print('Cheese')
+
+
+# while True:
+#    answer = input('say when: ')
+#    if answer.lower() == 'when':
+#       break
+# print('cheese')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/labs/py102.py
@@ -0,0 +1,45 @@
+# days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+# print(days[4])
+
+# my_list = ["horse", "dog", "cat", "pot belly pig"]
+# my_list_2 = ["goose", "chicken", "cow", "rooster"]
+
+# #print(my_list[1])
+# # my_list[1] = "wolf"
+# # my_list[3] = "owl"
+# #print(my_list[1])
+# print(my_list)
+
+# #print(len(my_list))
+# my_list.append(my_list_2)
+# my_list.extend(my_list_2)
+# print(my_list)
+
+# todos = ['pet the cat','water the plants','cook dinner']
+# index = 0
+
+# complete = ''
+# while True:
+#     newTodo = input("Enter a to do item ('stop' to complete): ")
+#     if newTodo.lower() == 'stop':
+#         break
+#     todos.append(newTodo)
+
+# while index < len(todos):
+#     print(f"{index}: {todos[index]}")
+#     index += 1
+
+nums = [1, 2, 3, 4, 5]
+
+new_list = nums[0:3]
+print(new_list)
+
+
+
+
+
+
+
+
+
+
