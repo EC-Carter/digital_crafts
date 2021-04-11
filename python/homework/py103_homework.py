d04751bcc497e5840c2a1eb2f8d6d3b58458d1a3
EC Carter <eca21988@gmail.com>
2021-04-10 22:56:02 -0400

    functions homework

---
+++ python/homework/py103_homework.py
@@ -0,0 +1,50 @@
+# 1 smallest number
+# numList = [4,7,9,-1,8,-12]
+# numList2 = [123,234,234,987,567,345,9908,234,532]
+# def smallestNum(numbers):
+#     smallest = numbers[0]
+#     for i in range(len(numbers)):
+#         if numbers[i]  <  smallest:
+#             smallest = numbers[i]
+#     return smallest
+# smallestNumber = smallestNum(numList2)
+# print(smallestNumber)
+
+# #2 largest number
+# numList = [4,7,9,-1,8,-12]
+# numList2 = [123,234,234,987,567,345,9908,234,532]
+# def largestNum(numbers):
+#     largest = numbers[0]
+#     for i in range(len(numbers)):
+#         if numbers[i]  >  largest:
+#             largest = numbers[i]
+#     return largest
+# largestNumber = largestNum(numList2)
+# print(largestNumber)
+
+#3. shortest string
+# words = ["cat", "dog", "z", "fish", "elephant"]
+# def shortestStr(strList):
+#     shortest = strList[0]
+#     for i in range(len(strList)):
+#         if len(strList[i])  <  len(shortest):
+#             shortest = strList[i]
+#     return shortest
+# shortS = shortestStr(words)
+# print(shortS)
+
+# 4. Longest string
+# words = ["cat", "dog", "fish", "elephant"]
+# def longestStr(strList):
+#     longest = strList[0]
+#     for i in range(len(strList)):
+#         if len(strList[i]) >  len(longest):
+#             longest = strList[i]
+#     return longest
+# longS = longestStr(words)
+# print(longS)
+
+
+
+
+
